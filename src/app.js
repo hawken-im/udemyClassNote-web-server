31c542866a8861789b9c32eb278bd36305e8fe5d
hawken <askhaokun@gmail.com>
2022-11-14 18:00:31 +0800

    keep going

--- src/app.js
+++ src/app.js
@@ -7,10 +7,32 @@ const res = require('express/lib/response')
 const publicDirectoryPath = path.join(__dirname,'../public')
 const app = express()
 
+app.set('view engine','hbs')
+
 app.use(express.static(publicDirectoryPath))
 
 //app.use(express.static)
 
+app.get('',(req,res)=>{
+    res.render('index',{
+        title:'Weather App',
+        name:'Junkdog'
+    })
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title:'About Me',
+        name:'Hawken'
+    })
+})
+
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        title:'Help',
+        helpText:'this is help text'
+    })
+})
 
 app.get('/weather',(_,res)=>{
     //console.log('some request:',req)
