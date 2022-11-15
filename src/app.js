051bcc7f9294c41cc21c405aa9672179c5f78cdb
hawken <askhaokun@gmail.com>
2022-11-15 11:20:39 +0800

    don't leave it to tomorrow

--- src/app.js
+++ src/app.js
@@ -1,22 +1,27 @@
 const path = require('path')
 const express = require('express')
 const res = require('express/lib/response')
+const hbs = require('hbs')
 
-
-
-const publicDirectoryPath = path.join(__dirname,'../public')
 const app = express()
 
+//Define paths for Express config
+const publicDirectoryPath = path.join(__dirname,'../public')
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname,'../templates/partials')
+
+//Setup hbs engine ande views location
 app.set('view engine','hbs')
+app.set('views',viewsPath)
+hbs.registerPartials(partialsPath)
 
+//Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
-
-//app.use(express.static)
 
 app.get('',(req,res)=>{
     res.render('index',{
         title:'Weather App',
-        name:'Junkdog'
+        name:'Hawken'
     })
 })
 
@@ -30,7 +35,8 @@ app.get('/about',(req,res)=>{
 app.get('/help',(req,res)=>{
     res.render('help',{
         title:'Help',
-        helpText:'this is help text'
+        helpText:'this is help text',
+        name:'Hawken'
     })
 })
 
